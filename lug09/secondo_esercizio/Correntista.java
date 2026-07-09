f24ab4d7e4b8f682c12f8977a6b45a46dbb9bcb5
mgigante-cell <mgigante@studenti.apuliadigital.it>
2026-07-09 12:45:20 +0200

    Add files via upload

---
+++ lug09/secondo_esercizio/Correntista.java
@@ -0,0 +1,25 @@
+package secondo_esercizio;
+
+public class Correntista {
+    private static int idProgressivo = 0;
+    
+    private final int ID;
+    private String nome;
+    private ContoBancario conto;
+    
+
+    public Correntista(String nome){
+        this.ID = ++idProgressivo;
+        this.nome = nome;
+        conto = new ContoBancario();
+    }
+
+    public int getID() { return ID; }
+    public String getNome() { return nome; }
+    public ContoBancario getConto() { return conto; }
+
+    @Override
+    public String toString() {
+        return "Correntista [ID=" + ID + ", nome=" + nome + ", conto=" + conto + "]";
+    }
+}
